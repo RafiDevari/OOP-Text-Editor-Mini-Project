3f6af01bb381a2e4f15d27abf8656de07b86788e
M-Thoriq <mthoriqalasyjari@gmail.com>
2023-05-31 13:49:17 +0700

    third commit

---
+++ color.hpp
@@ -0,0 +1,55 @@
+/*
+Nama File : color.hpp
+Deskripsi : File ini berisi fungsi untuk mengubah warna tampilan teks editor pada terminal.
+            File ini di include di textinteface.hpp.
+*/ 
+#include <iostream>
+using namespace std;
+
+//class Color yang berisi fungsi ubah tema dan fungsi diturunkan ke 
+//class Tampilan di textinterface.hpp
+class Color     
+{
+    public :
+        int theme;
+        void chooseTheme();     //fungsi untuk menerima input theme untuk memilih tema
+        void editorTheme();     //fungsi membaca input theme dan mengubah warnanya
+        int getInfoColor();     //untuk melakukan tes disaat program ini dibuat
+        void tampilanSetting(); //untuk melakukan tes disaat program ini dibuat
+};
+
+void Color::chooseTheme()
+{
+    cout << "Warna apa maunya?\n";  //menu meminta input theme
+    cout << "1. Dark Mode\n";
+    cout << "2. Light Mode\n";
+    cout << "3. Default\n";
+    cout << "Pilihan: ";
+    cin >> theme;
+}
+
+void Color::editorTheme()
+{
+        switch (theme)  //switch case untuk mengubah warna sesuai dengan input theme
+        {
+        case 1:     //warna backgroud gelap dengan teks hijau
+            cout << "\033[1;32m";   
+            cout << "\033[40m";
+            break;
+        case 2:     //warna backgroud putih dengan teks pink
+            cout << "\033[1;95m";   
+            cout << "\033[107m";
+            break;
+        case 3:     //warna terminal default
+            cout << "\033[1;13";
+            cout << "\033[64m";
+            break;
+        default:    //jika menginput tidak sesuai menu yang tersedia
+            break;
+        }
+}
+
+int Color::getInfoColor()
+{
+    return theme;       //untuk melakukan tes disaat program ini dibuat
+}
---
+++ textinterface.hpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <windows.h>
+#include <iostream>
+#include "color.hpp"
+
+using namespace std;
+
+class Tampilan : public Color //Inherit class dari library buatan bernama COLOR
+{
+    private :
+        int width, height;
+    public :
+        CONSOLE_SCREEN_BUFFER_INFO csbi;
+        
+        Tampilan(){  //Constructor untuk memulai program yang menyesuaikan ukuran window
+            GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+            width = csbi.srWindow.Right - csbi.srWindow.Left;
+            height = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+        }
+        void printCtr(string);
+        void right_left();
+        void colorSetting(){chooseTheme();}
+        void garis();
+        void kosong();
+        ~Tampilan(){cout << "\033[39m" << "\033[49m";}
+};
+
+void Tampilan::garis()     //membuat semacam suatu barrier yang membatasi atas dan bawah program
+{
+    for (int i = 0; i < width; i++)  //akan terus mencetak "=" sampai lebar terakhir dari program
+    {
+        cout << "=";
+    } cout << endl;
+}
+
+void Tampilan::kosong()  //membuat "|" diawal lalu membuat "|" di lebar akhir dari program
+{
+    cout << "|";
+    for (int i = 0; i < width-2; i++)
+    {
+        cout << " ";
+    }
+    cout << "|";
+    cout << endl;
+}
+
+void Tampilan::printCtr(string text)   //akan membuat Text sesuai dengan isi argumen di tengah program
+{
+    int len;
+    len = text.length();
+    if (len <= 8)
+    {
+        cout << "|" ;
+        for (int i = 0; i < (width/2)-2; i++)
+        {
+            cout << " ";
+        }
+        cout << text;
+        
+        for (int i = 0; i <= width/2-(len); i++)
+        {
+            cout << " ";
+        }
+        cout << "|" << endl;
+    }
+    else if (len >=9)
+    {
+        cout << "|";
+        for (int i = 0; i < (width/2-(len/2)); i++)
+        {
+            cout << " ";
+        }
+        cout << text;
+        for (int i = 0; i <= width/2-(len/2)-3; i++)
+        {
+            cout << " ";
+        }
+        cout << "|" << endl;
+    }
+
+}
+
